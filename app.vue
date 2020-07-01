2c17ec7b3486b7f1c95daaf7ccd234e1c7b36dfd
duyi-gerrard8 <253328850@qq.com>
2020-07-01 21:06:32 +0800

    45

---
+++ app.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        xxx
+        {{ msg }}
+        <p>我是app组建的p元素</p>
+        <base-son/>
+    </div>
+</template>
+
+<script>
+    import baseSon from './son';
+    export default {
+        name: "app",
+        components: {
+            baseSon
+        },
+        data() {
+            return {
+                msg: 'hello world'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: red;
+    }
+</style>
---
+++ son.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        son
+        <p>hahaha</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "son",
+        data() {
+
+        }
+    }
+</script>
+
+<style scoped>
+p{
+    background-color: green;
+}
+</style>
