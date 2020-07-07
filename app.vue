d608f5db3c572db78ac30b10efc257c72d88e864
duyi-gerrard8 <253328850@qq.com>
2020-07-07 17:21:21 +0800

    49

--- app.vue
+++ app.vue
@@ -1,28 +1,78 @@
 <template>
-    <div class="app">
-        <!--<date-picker :date="date" @choose-date="chooseDate"/>-->
-        <date-picker v-model="date"/>
+    <div id="app">
+        <base-tree :data="data"/>
     </div>
 </template>
 
 <script>
-    import DatePicker from './DatePicker'
+    import BaseTree from './tree'
 
     export default {
         name: "app",
         components: {
-            DatePicker,
+            BaseTree,
         },
         data() {
             return {
-                date: new Date()
+                data: [
+                    {
+                        label: "一级 1",
+                        children: [
+                            {
+                                label: "二级 1-1",
+                                children: [
+                                    {
+                                        label: "三级 1-1-1"
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        label: "一级 2",
+                        children: [
+                            {
+                                label: "二级 2-1",
+                                children: [
+                                    {
+                                        label: "三级 2-1-1"
+                                    }
+                                ]
+                            },
+                            {
+                                label: "二级 2-2",
+                                children: [
+                                    {
+                                        label: "三级 2-2-1"
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        label: "一级 3",
+                        children: [
+                            {
+                                label: "二级 3-1",
+                                children: [
+                                    {
+                                        label: "三级 3-1-1"
+                                    }
+                                ]
+                            },
+                            {
+                                label: "二级 3-2",
+                                children: [
+                                    {
+                                        label: "三级 3-2-1"
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
             }
-        },
-        // methods: {
-        //     chooseDate(date) {
-        //         this.date = date;
-        //     }
-        // }
+        }
     }
 </script>
 
